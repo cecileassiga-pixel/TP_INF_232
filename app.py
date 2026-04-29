7db7b7468e837916e02f3174bd27b571cfc373d0
cecileassiga-pixel <cecileassiga@gmail.com>
2026-04-29 08:20:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-DB_PATH = "database.db"
+import os
+DB_PATH = os.path.join(os.environ.get("TMPDIR", "/tmp"), "database.db")
 
 def get_db():
     conn = sqlite3.connect(DB_PATH)
